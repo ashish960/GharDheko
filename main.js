02b33984f781d3502d21b1f783f19dfe2e17e89e
Ashish painuly <85127007+ashish960@users.noreply.github.com>
2023-02-25 11:52:39 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+
+let a=document.getElementById("searchButton");
+a.onclick = function () {
+    console.log("it is correctly working");
+    
+    location.href="/searchRoom.html";
+};
+
+  
+    
